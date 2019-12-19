d8c0bb4805d8a3cfc9fd8abc32f29e2f73048702
ranlin520 <1368590868@qq.com>
2019-12-19 13:57:15 +0800

    'pinxianimages存储仓库'

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const QINIU = {
+  accessKey: "mb4u32kYPqC_6DHkjtnmPi0hFandO_MXWBAeyg4R",
+  secretKey: "-ABx7ukAfHfP8eLLgKOtGw8RqXRleInc5Eg9a6jq",
+  bucket: "pinxianimages",
+  origin: "q2ozjg5p9.bkt.clouddn.com",
+};
+
+module.exports = QINIU
